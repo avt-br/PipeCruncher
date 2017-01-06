7b72560e457bb3c161acd05a412fb2d33c5d4917
Pedro Santos <milheiro.santos@gmail.com>
2017-01-06 18:41:33 +0100

    update; change of mechanics

--- game.js
+++ game.js
@@ -1,6 +1,59 @@
-var first_click = null;
-var second_click = null;
 
+function pointsObjective(nbPoints) {
+  return function(){
+   return get_score() >= nbPoints;
+  };
+}
+
+function createSimpleBoard()
+{
+   for(var i=0; i<nb_rows; ++i){
+     for(var j=0; j<nb_cols; ++j){
+       set_pipe(index_to_pipe(i, j), random_pipe_name());
+     }
+   }
+}
+
+function popup(text, button_text, callback){
+  document.getElementById("popup_text").innerHTML = text;
+  document.getElementById("popup_button").innerHTML = button_text;
+  document.getElementById("popup").style.display = "block";
+  document.getElementById("popup_button").addEventListener("click",
+      function popup_callback(){
+        document.getElementById("popup").style.display = "none";
+        document.getElementById("popup_button").removeEventListener("click", popup_callback);
+        if(callback){
+          callback();
+        }
+      });
+}
+var activeLevel = 0; 
+
+var levels = [];
+levels[0] = {
+  nbMoves: 30,
+  completedObjective: pointsObjective(1000),
+  boardGenerator: createSimpleBoard,
+  description: "Reach 1000 Points!"
+}
+levels[1] = {
+  nbMoves: 30,
+  completedObjective: pointsObjective(2000),
+  boardGenerator: createSimpleBoard,
+  description: "Reach 2000 Points!"
+}
+levels[2] = {
+  nbMoves: 40,
+  completedObjective: pointsObjective(3000),
+  boardGenerator: createSimpleBoard,
+  description: "Reach 3000 Points!"
+}
+levels[3] = {
+  nbMoves: 99999999999,
+  completedObjective: function(){return false;},
+  boardGenerator: createSimpleBoard,
+  description: "Sandbox Mode!\nHave Fun!"
+}
 var nb_rows = 9;
 var nb_cols = 11;
 
@@ -12,14 +65,15 @@ var pipe_names = [
     "corner_top_left",
     "corner_bottom_right",
     "corner_bottom_left",
-    "sink_right",
-    "source_left",
-    "empty"
+    "empty",
+    "t_junction_right_down_left",
+    "t_junction_down_left_top",
+    "t_junction_left_top_right",
+    "t_junction_top_right_down"
 ];
 
 var static_pipe_names = [
-    "sink_right",
-    "source_left"
+  "boulder"
 ];
 
 var play_pipe_names = [
@@ -29,17 +83,41 @@ var play_pipe_names = [
     "corner_top_right",
     "corner_top_left",
     "corner_bottom_right",
-    "corner_bottom_left"
+    "corner_bottom_left",
+    "t_junction_right_down_left",
+    "t_junction_down_left_top",
+    "t_junction_left_top_right",
+    "t_junction_top_right_down"
 ];
 
-function add_score(partial){
-  var e = document.getElementById("score")
-  e.innerHTML = "" + (parseInt(e.innerHTML) + partial);
+function set_level(new_level){
+  var e = document.getElementById("level");
+  e.innerHTML = "" + new_level;
 }
 
-function set_score(partial){
+function get_level(){
+  var e = document.getElementById("level");
+  return parseInt(e.innerHTML); 
+}
+
+function set_moves(new_moves){
+  var e = document.getElementById("moves");
+  e.innerHTML = "" + new_moves;
+}
+
+function get_moves(){
+  var e = document.getElementById("moves");
+  return parseInt(e.innerHTML); 
+}
+
+function set_score(new_score){
   var e = document.getElementById("score");
-  e.innerHTML = "" + partial;
+  e.innerHTML = "" + new_score;
+}
+
+function get_score(){
+  var e = document.getElementById("score");
+  return parseInt(e.innerHTML); 
 }
 
 function get_pipe_url(pipe_name, ext){
@@ -81,26 +159,11 @@ function index_to_pipe(idx1, idx2){
   return document.getElementById("idx_"+idx1+"_"+idx2);
 }
 
-function new_game(){
-
+function new_game(level){
   set_score(0);
-
-  //first column are the sources
-  for(var i=0; i<nb_rows; ++i){
-    set_pipe(index_to_pipe(i, 0), "source_left");
-  }
-  //last column are the sources
-  for(var i=0; i<nb_rows; ++i){
-    set_pipe(index_to_pipe(i, nb_cols-1), "sink_right");
-  }
-  //rest is random
-  for(var i=0; i<nb_rows; ++i){
-    for(var j=1; j<nb_cols-1; ++j){
-      set_pipe(index_to_pipe(i, j), random_pipe_name());
-    }
-  }
-
-  reset_water();
+  set_moves(level.nbMoves);
+  level.boardGenerator();
+  popup(level.description, "Go");
 }
 
 function rotate(element){
@@ -138,17 +201,20 @@ function rotate(element){
 }
 
 function destroy_pipes(elements, callback){
+  var partial_score = elements.length*10;
+  set_score(get_score()+partial_score);
+  
   //Make an explosion!
-  elements.forEach(function(e){
-    var pipe_url = "url(\"Images/explosion.gif\")";
-    e.style.background = pipe_url;
-  });
+  var explosion_url = "url(\"Images/explosion.gif\")";
+  for(var i=0; i<elements.length; ++i){
+    elements[i].style.background = explosion_url;
+  }
   //Set elements to empty
   setTimeout(function(){
-    elements.forEach(function(e){
-      set_pipe(e, "empty");
-    });
-    callback();
+    for(var i=0; i<elements.length; ++i){
+      set_pipe(elements[i], "empty");
+    }
+    push_down_board(callback);
   }, 450);
 }
 
@@ -206,11 +272,27 @@ function get_pipe_connections(e){
     return {top:true, bottom:true, left:false, right:true};
   }
     
-  return {top:false, bottom:false, left:false, right:true};
+  if(name == "t_junction_right_down_left"){
+    return {top:false, bottom:true, left:true, right:true};
+  }
+  
+  if(name == "t_junction_down_left_top"){
+    return {top:true, bottom:true, left:true, right:false};
+  }
+
+  if(name == "t_junction_left_top_right"){
+    return {top:true , bottom:false, left:true, right:true};
+  }
+  
+  if(name == "t_junction_top_right_down"){
+    return {top:true , bottom:true, left:false, right:true};
+  }
+
+    return {top:false, bottom:false, left:false, right:false};
 }
 
 function are_pipes_connected(e1, e2){
-  if (!e1 || !e2 || !are_pipes_adjacent) return false;
+  if (!e1 || !e2 || !are_pipes_adjacent(e1,e2)) return false;
 
   var idx1 = get_pipe_index(e1);
   var idx2 = get_pipe_index(e2);
@@ -248,7 +330,6 @@ function mark_connected_pipes(){
   ];
 
   function search_index(e, idx1, idx2){
-    var adj = index_to_pipe(idx1, idx2);
     if (adj && !searched[idx1][idx2] && are_pipes_connected(e, adj)){
       search(adj);
     }
@@ -287,7 +368,6 @@ function reset_water(){
       mark_no_water(index_to_pipe(i,j));
     }
   }
-
   mark_connected_pipes();
   destroy_percolated_pipes();
 }
@@ -313,17 +393,13 @@ function destroy_percolated_pipes(){
       }
     }
   }
-  destroy_pipes(toDestroy, function(){
-    add_score(partial_score);
-    reset_water();
-    push_down_board();
-  });
+  destroy_pipes(toDestroy)
 }
 
-function push_down_board(){
+function push_down_board(callback){
   top_is_empty=false;
   for(var i=nb_rows-1; i>=0; --i){
-    for(var j=1; j<nb_cols-1; ++j){
+    for(var j=0; j<nb_cols; ++j){
       var e1 = index_to_pipe(i, j);
       var e2 = index_to_pipe(i+1, j);
 
@@ -334,7 +410,7 @@ function push_down_board(){
     }
   }
   var top_is_empty = false;
-  for(var j=1;j<nb_cols-1;++j){
+  for(var j=0;j<nb_cols;++j){
     var e = index_to_pipe(0, j);
     if(get_pipe_name(e) == "empty"){
       top_is_empty = true;
@@ -342,17 +418,59 @@ function push_down_board(){
     } 
   }
   if(top_is_empty){
-    setTimeout(push_down_board, 250);
-  }else{
-    reset_water();
+    setTimeout(function(){push_down_board(callback);}, 250);
   }
+  else if(callback) {
+    callback();
+  }
+}
+function game_over(){
+  popup("Game Over :(", "Restart", function(){ new_game(levels[activeLevel]); });
 }
 
 document.addEventListener("DOMContentLoaded", function(event) { 
-  new_game();
-  document.getElementById("restart").addEventListener("click", new_game);
-  cells = document.querySelectorAll("#game_table td");
+  new_game(levels[activeLevel]);
+  document.getElementById("restart").addEventListener("click", function(){new_game(levels[activeLevel])});
+  var cells = document.querySelectorAll("#game_table td");
+  var mouse_down = false;
+  var selected_pipes = [];
   for(var i=0; i<cells.length; ++i){
-    (cells[i]).addEventListener("click", function(ev){rotate(ev.currentTarget)});
+    (cells[i]).addEventListener("mousedown", function(ev){
+      mouse_down = true;
+      last_pipe = this_pipe;
+      mark_has_water(this_pipe);
+      selected_pipes.push(this_pipe);
+    });
+    (cells[i]).addEventListener("mouseover", function(ev){
+      this_pipe = ev.currentTarget;
+      if(mouse_down && selected_pipes.length > 0 
+          && selected_pipes.indexOf(this_pipe) == -1
+          && are_pipes_connected(this_pipe, selected_pipes[selected_pipes.length-1])){
+        mark_has_water(this_pipe);
+        selected_pipes.push(this_pipe);
+      };
+    });
+    (cells[i]).addEventListener("mouseup", function(ev){
+      mouse_down = false;
+      for(var i=0; i<selected_pipes.length; ++i){
+        mark_no_water(selected_pipes[i]);
+      }
+      if(selected_pipes.length >= 3)
+      {
+        destroy_pipes(selected_pipes, function(){
+          set_moves(get_moves()-1);
+          if(levels[activeLevel].completedObjective()){
+            popup("Congratulations, you won the level", "Next Level", function(){
+              ++activeLevel;
+              new_game(levels[activeLevel]);
+            });
+          }
+          else if(get_moves() <= 0) {
+            game_over();
+          }
+        });
+      }
+      selected_pipes = [];
+    });
   }
 });
